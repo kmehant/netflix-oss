9a4132cc9b797b489ad6d2ebd02b7ab6274f6233
K mehant <411843@student.nitandhra.ac.in>
2021-05-27 14:39:35 +0530

    push to master add config server

---
+++ ms-config-server/src/test/java/ro/pentalog/pentabar/ms/MsConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package ro.pentalog.pentabar.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
